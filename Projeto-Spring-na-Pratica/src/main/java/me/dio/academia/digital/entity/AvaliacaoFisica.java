5d6fc3d2ccf3ea9ee05e750f3f3cdac3e4a92b8b
André Negreiros <112659872+Andrengr0@users.noreply.github.com>
2023-04-14 15:09:52 -0300

    corrections

--- Projeto-Spring-na-Pratica/src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ Projeto-Spring-na-Pratica/src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -1,12 +1,19 @@
 package me.dio.academia.digital.entity;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
